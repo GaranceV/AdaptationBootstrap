ecc0ac3629b616ad4c680ef4697421eff032bb81
Garance <garance.vallat@gmail.com>
2015-10-13 17:38:14 +0200

    cree une version 0 pour une seule etape

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AdaptationBootstrap
 Adaptation of HMI project, based on bootstrap responsive design
+
+
+## Lancement
+Pour lancer le projet, la première fois : 
+`npm install`
+`bower install`
+Suite à cela, et toutes les autres fois : 
+`grunt serve`
+
+Le projet va alors se lancer sur localhost, sur le port 1818. 
+On peut modifier ce paramètre dans gruntfile.js, à la racine du projet. 
+
+## Responsive design
+Ce projet est prévu pour s'adapter aux différents usage qu'on peut faire d'une page contenant une recette de cuisine. 
+Si vous le regarder au format mobile, vous ne verrez que la liste des ingrédients. 
+Au format tablette, on ne voit plus que les étapes de la recette
+Au format bureau, on voit la liste des ingrédients et les étapes, en vue d'ensemble. 
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,52 +2,52 @@
  * Created by Garance on 11/10/2015.
  */
 var recipeData = {
-    recette:{
-        nom:"muffin",
-        etapes:[
+    recette: {
+        nom: "muffin",
+        etapes: [
             {
-                ordre:"1",
-                contenu:"faire fondre beurre et chocolat",
-                ingredients:[
+                ordre: "1",
+                contenu: "faire fondre beurre et chocolat",
+                ingredients: [
                     {
-                        index:"1"
+                        index: "1"
                     },
                     {
-                        index:"2"
+                        index: "2"
                     }
                 ]
             },
             {
-                ordre:"2",
-                contenu:"mélanger avec le sucre",
-                ingredients:[
+                ordre: "2",
+                contenu: "mélanger avec le sucre",
+                ingredients: [
                     {
-                        index:"1"
+                        index: "1"
                     },
                     {
-                        index:"2"
+                        index: "2"
                     },
                     {
-                        index:"3"
+                        index: "3"
                     }
                 ]
             }
         ],
-        ingredients:[
+        ingredients: [
             {
-                index:"1",
-                nom:"beurre",
-                quantite:"120g"
+                index: "1",
+                nom: "beurre",
+                quantite: "120g"
             },
             {
-                index:"2",
-                nom:"chocolat",
-                quantite:"200g"
+                index: "2",
+                nom: "chocolat",
+                quantite: "200g"
             },
             {
-                index:"3",
-                nom:"sucre",
-                quantite:"100g"
+                index: "3",
+                nom: "sucre",
+                quantite: "100g"
             }
         ]
     }
@@ -58,29 +58,61 @@ console.log("hors de tout");
 var ingredientsCounter = 0;
 var tbody = document.getElementById('ingredientsList');
 
-var printIngredients = function(myRecipe) {
-    for (var i = 0; i < myRecipe.ingredients.length; i++) {
+var ingredientsTable = recipeData.recette.ingredients;
+var ingredientLookup = {};
+
+var printIngredients = function () {
+    for (var i = 0; i < ingredientsTable.length; i++) {
+        //We create a look up table at the same time, for future use
+        console.log(ingredientLookup);
+        ingredientLookup[ingredientsTable[i].index] = ingredientsTable[i].nom;
+        console.log('coucou', i);
         console.log('un ingredient !');
         var tr = "<tr>";
 
         /* Must not forget the $ sign */
-        tr += "<td>" + myRecipe.ingredients[i].nom + "</td>" + "<td>" + myRecipe.ingredients[i].quantite + "</td></tr>";
+        tr += "<td>" + ingredientsTable[i].nom + "</td>" + "<td>" + ingredientsTable[i].quantite + "</td></tr>";
 
         /* We add the table row to the table body */
         tbody.innerHTML += tr;
     }
-    };
+};
+
 
 function load() {
     console.log("coucou");
- //   var myRecipe = JSON.parse(recipeData);
-    var myRecipe = recipeData.recette;
-    console.log(myRecipe.ingredients);
-    document.getElementById('nomRecette').innerHTML = myRecipe.nom;
-    printIngredients(myRecipe);
+    //   var myRecipe = JSON.parse(recipeData);
+    document.getElementById('nomRecette').innerHTML = recipeData.recette.nom;
+    printIngredients();
+
 }
 load();
 
 
 //On ajoute une fonction "fillCarousel"
 
+//Pour ajouter la liste d'ingrédients de l'étape dans le cas de la tablette
+var makeUL = function(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for (var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+        var ingredient = ingredientLookup[array[i].index];
+        // Set its contents:
+        item.appendChild(document.createTextNode(ingredient));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+};
+
+// Add the contents of options[0] to #foo:
+//can this function be used to fill every part of the carousel through a loop ?
+//document.getElementsByClassName()
+document.getElementById('ingredientList').appendChild(makeUL(recipeData.recette.etapes[0].ingredients));
+document.getElementById('stepContent').innerHTML  = recipeData.recette.etapes[0].contenu;
